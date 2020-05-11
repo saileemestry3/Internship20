da876b5423f7dc9bc0c6591f4894215a8119d268
saileemestry3 <saileemestry433@gmail.com>
2020-05-11 18:12:46 +0530

    assignment07

---
+++ python/assignment 07.py
@@ -0,0 +1,5 @@
+print("Addition of two complex numbers : ",(4+3j)+(3-7j))
+print("Subtraction of two complex numbers : ",(4+3j)-(3-7j))
+print("Multiplication of two complex numbers : ",(4+3j)*(3-7j))
+print("Division of two complex numbers : ",(4+3j)/(3-7j))
+print("Modulus of two complex numbers :",(4+3)%(3-7))
